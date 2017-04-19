0120fa9a8c4d397015d813caabab0d20065f5dc8
Matyas Fodor <matyas.fodor@gmail.com>
2017-04-19 23:52:30 +0200

    Create target and render escher in that target.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Builder} from 'escher-vis';
+import { select, Selection, BaseType} from 'd3-selection';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
-  builder = Builder(null, null, null, null, {});
+export class AppComponent implements OnInit{
+  title: string;
+  target: Selection<BaseType, {}, null, undefined>;
+  builder: any;
+  constructor() {
+    this.title = 'app works!';
+  }
+  ngOnInit() {
+    this.target = select('#escher-target');
+    // I had issues with jQuery, and there was another issue, possibly a bug
+    // The `{menu: 'zoom'}` option is a workaround.
+    this.builder = Builder(null, null, null, this.target, {menu: 'zoom'});
+  }
 }

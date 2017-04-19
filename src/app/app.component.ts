649c340f3321d6679756e8988ee3684979847c6b
Matyas Fodor <matyas.fodor@gmail.com>
2017-04-19 21:39:51 +0200

    Import and initialize builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Builder} from 'escher-vis';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  builder = Builder(null, null, null, null, {});
 }

0438a25087365061a4d4f95886ec385b9970f186
Matyas Fodor <matyas.fodor@gmail.com>
2017-04-21 20:05:31 +0200

    Add pipe in order to iterate over objects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ObjectPipe } from './app.pipes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObjectPipe
   ],
   imports: [
     BrowserModule,

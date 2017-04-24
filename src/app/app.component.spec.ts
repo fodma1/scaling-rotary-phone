b1cdaf04dec3ebd9ce33fc96fe3fecbe6789dd17
Matyas Fodor <matyas.fodor@gmail.com>
2017-04-24 08:53:37 +0200

    Fixed color scheme tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,15 +20,19 @@ describe('AppComponent', () => {
   it(`should have false greenScheme by default`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+    const target = fixture.debugElement.query(By.css('#escher-target'));    
     expect(app.greenScheme).toBeFalsy();
+    expect(('green-scheme' in target.classes)).toBeFalsy();
   }));
 
   it(`should toggle greenScheme when the button is clicked`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     const colorToggle  = fixture.debugElement.query(By.css('#colorSchemeButton'));
+    const target = fixture.debugElement.query(By.css('#escher-target'));    
     colorToggle.triggerEventHandler('click', null);
     fixture.detectChanges();
     expect(app.greenScheme).toBeTruthy();
+    expect(('green-scheme' in target.classes)).toBeTruthy();    
   }));
 });
